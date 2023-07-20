8adc1c510af974cedc07abcbc61e2a931f67f15c
Igor Oliveira <igoroliveira9827@gmail.com>
2023-07-20 16:59:51 -0300

    build: create routes document

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+const route = Router();
+
+route.get("/", (req, res) => {
+    res.json({
+        message: "Hello World!"
+    })
+});
+
+route.get("/teste", (req, res) => {
+    res.send("Rota teste funcionando!");
+});
+
+export default route;
