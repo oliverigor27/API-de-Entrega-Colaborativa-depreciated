b217412305ef1032b0ac1a6f6634f9cfda381117
Igor Oliveira <igoroliveira9827@gmail.com>
2023-07-20 16:59:42 -0300

    refactor: move route for route's archive and add a new port option on listen event

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import {config} from 'dotenv';
+import cors from 'cors'
+
+import route from './routes'
 
 config()
 
 const app = express();
+
 app.use(express.json());
+app.use(route);
+app.use(cors);
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "HellO World!"
-    });
-});
-
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3030, () => {
     console.log("Server is running!");
 });
