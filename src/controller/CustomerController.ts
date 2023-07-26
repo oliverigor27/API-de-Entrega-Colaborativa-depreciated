bc46f3b12112557c25d37fca902f1dcb722be7d5
Igor Oliveira <igoroliveira9827@gmail.com>
2023-07-26 19:47:44 -0300

    fix: remove customerModel

--- src/controller/CustomerController.ts
+++ src/controller/CustomerController.ts
@@ -3,33 +3,37 @@ import CustomerModel from "../model/CustomerModel";
 
 
 class CustomerController {
-    private customerModel = CustomerModel;
 
-    async CreateCustomer(req: Request, res: Response) {
+    async CreateNewCustomer(req: Request, res: Response) {
         try {
+
             const { customer_name,
-                customer_adress,
-                customer_city,
                 customer_email,
                 customer_password,
+                customer_adress,
+                customer_city,
                 customer_state } = req.body;
     
-            this.customerModel.createCustomer( customer_name,
-                customer_adress,
-                customer_city,
+            await CustomerModel.createCustomer( customer_name,
                 customer_email,
                 customer_password,
+                customer_adress,
+                customer_city,
                 customer_state );
     
             res.status(200).json({
-                message: "User create into database!"
+                message: "User create into database!",
             });
         } catch (error) {
-            res.json({
+            res.status(500).json({
                 message: "Error when tried to create a new customer in database"
             })
             console.error(error);
         }
+    }
+
+    async getCustomers() {
+        return await CustomerModel.getCustomer();
     }
 }
 
