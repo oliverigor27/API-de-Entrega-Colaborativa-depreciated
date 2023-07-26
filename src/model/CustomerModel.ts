ec14736940002d522f73ed1e04a1c407c54d80ea
Igor Oliveira <igoroliveira9827@gmail.com>
2023-07-26 18:36:26 -0300

    refactor: adding new method in Customer's class model

--- src/model/CustomerModel.ts
+++ src/model/CustomerModel.ts
@@ -6,6 +6,23 @@ class CustomerModel {
     private express = express();
     private prisma = prisma;
 
+    async createCustomer(customer_name: string, customer_adress: string, customer_city: string, customer_email: string, customer_password: string, customer_state: string) {
+
+        const customers = await this.prisma.customer.create({
+            data: {
+                customer_name,
+                customer_adress,
+                customer_city,
+                customer_email,
+                customer_password,
+                customer_state
+            }
+        });
+
+        return customers;
+    }
+
+
 }
 
 export default new CustomerModel();
