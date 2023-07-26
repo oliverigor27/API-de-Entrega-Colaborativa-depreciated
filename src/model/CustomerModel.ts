ec89de6ca5a6b7c4c37f39212220cba62f49b3fa
Igor Oliveira <igoroliveira9827@gmail.com>
2023-07-26 19:48:24 -0300

    fix: remove prisma and import PrismaClient

--- src/model/CustomerModel.ts
+++ src/model/CustomerModel.ts
@@ -1,28 +1,24 @@
-import express from 'express';
-import prisma from '../services/prisma';
-
+import { prismaCliente } from '../services/prisma';
 
 class CustomerModel {
-    private express = express();
-    private prisma = prisma;
 
-    async createCustomer(customer_name: string, customer_adress: string, customer_city: string, customer_email: string, customer_password: string, customer_state: string) {
+    async createCustomer(customer_name: string, customer_email: string, customer_password: string,  customer_adress: string, customer_city: string, customer_state: string) {
 
-        const customers = await this.prisma.customer.create({
+        await prismaCliente.customer.create({
             data: {
                 customer_name,
-                customer_adress,
-                customer_city,
                 customer_email,
                 customer_password,
+                customer_adress,
+                customer_city,
                 customer_state
             }
         });
-
-        return customers;
     }
 
-
+    async getCustomer() {
+        return await prismaCliente.customer.findMany;
+    }
 }
 
 export default new CustomerModel();
